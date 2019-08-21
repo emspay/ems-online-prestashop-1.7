34a3b4539e2839089261742af7bffbc9a1902410
Maksim <maxim.volgust@leasoft.org>
2019-08-21 12:41:02 +0200

    Minor fixes

--- emspaysofort/translations/fr.php
+++ emspaysofort/translations/fr.php
@@ -2,7 +2,7 @@
 
 global $_MODULE;
 $_MODULE = array();
-$_MODULE['<{emspaysofort}prestashop>emspaysofort_77d972e09403cfdb1d3861bc978e0a1f'] = 'EMS Online SOFORT	';
+$_MODULE['<{emspaysofort}prestashop>emspaysofort_77d972e09403cfdb1d3861bc978e0a1f'] = 'EMS Online SOFORT';
 $_MODULE['<{emspaysofort}prestashop>emspaysofort_f34a3858b182ac360b1f979670db1af6'] = 'Accepter les versements en utilisant SOFORT dans votre boutique en ligne.';
 $_MODULE['<{emspaysofort}prestashop>emspaysofort_068bcd34a6ded96b1805ca0b9a6745bb'] = 'Payer par SOFORT';
 $_MODULE['<{emspaysofort}prestashop>error_68a489dbc8079a40832c3d558fcfb069'] = 'Votre commande à %s';
