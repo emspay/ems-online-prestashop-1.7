f91c8507d34fce8b2abc34f13bbd48d67312057e
Denis Tsyganok <denis.tsyganok@leasoft.org>
2021-12-07 14:55:02 +0200

    Unified translation

--- emspaybanktransfer/translations/de.php
+++ emspaybanktransfer/translations/de.php
@@ -2,11 +2,6 @@
 
 global $_MODULE;
 $_MODULE = array();
-$_MODULE['<{emspaybanktransfer}prestashop>emspaybanktransfer_e7ec7bf7d9dc25c33b09f6ea03905fb3'] = 'EMS Online Banküberweisung';
-$_MODULE['<{emspaybanktransfer}prestashop>emspaybanktransfer_e103c52e308e5e3b705906aa73165388'] = 'Akzeptiere Zahlungen für Ihre Produkte mit EMS Online Banküberweisung.';
-$_MODULE['<{emspaybanktransfer}prestashop>emspaybanktransfer_572b221ec653032250100cefd10a31eb'] = 'Bezahlen bei Banküberweisung';
-$_MODULE['<{emspaybanktransfer}prestashop>emspaybanktransfer_0d8a5a85d378a2c248326eac1b045556'] = 'Verwenden Sie die folgende Referenz, beim Zahlen für Ihre Bestellung:';
-$_MODULE['<{emspaybanktransfer}prestashop>emspaybanktransfer_218fa8ba064e0a843fb30108aeef5562'] = 'Zahlungsreferenz :';
 $_MODULE['<{emspaybanktransfer}prestashop>error_5023dbc72719b0d03162a18fda6db2b3'] = 'Leider ist ein Fehler bei der Verarbeitung Ihrer Bezahlung aufgetreten.';
 $_MODULE['<{emspaybanktransfer}prestashop>error_7dc1ee1a41f69f4027a773780c058b07'] = 'Bitte klick hier um eine andere Zahlungsart zu wahlen.';
 $_MODULE['<{emspaybanktransfer}prestashop>error_34e0a062726d06e24047764c3bef28c0'] = 'Bitte klicken Sie hier noch einmal zu versuchen.';
@@ -16,8 +11,10 @@ $_MODULE['<{emspaybanktransfer}prestashop>payment_return_39e50761df1515cf368f124
 $_MODULE['<{emspaybanktransfer}prestashop>payment_return_60cc46d58b23f7b478ffcc73ed70ae57'] = 'Ihrer Bezahlung bei %s wird bearbeitet.';
 $_MODULE['<{emspaybanktransfer}prestashop>payment_return_0aa295158c331b68aaa44f535abf0718'] = 'Um Ihre Bestellung abzuschließen, überweisen Sie den Betrag mit den untenstehenden Informationen';
 $_MODULE['<{emspaybanktransfer}prestashop>payment_return_6702a6e3bc2dce95c3e3b61fe578f29c'] = 'Betrag';
-$_MODULE['<{emspaybanktransfer}prestashop>payment_return_f818a9a94552277e13d28909ab3e1fa6'] = 'Kontoinhaber';
+$_MODULE['<{emspaybanktransfer}prestashop>payment_return_eaf764f7a047e4b1613d7019273b246b'] = 'Kontoinhaber:';
+$_MODULE['<{emspaybanktransfer}prestashop>payment_return_013b7b092450c19ce5dc8ac9d34407fb'] = 'Residenz:';
 $_MODULE['<{emspaybanktransfer}prestashop>payment_return_f6021c16ad0d13f2e26b1fd2fd62d5e3'] = 'IBAN';
+$_MODULE['<{emspaybanktransfer}prestashop>payment_return_a4b5dab963a7cd5ec945f7a53488bf00'] = 'BIC : ';
 $_MODULE['<{emspaybanktransfer}prestashop>payment_return_85bdf7b882f8de860295ee2698cf2ee2'] = 'Bitte vergessen Sie nicht, die Zahlungsreferenz %s in die Umschreibung Ihrer Banküberweisung einzufügen.';
 $_MODULE['<{emspaybanktransfer}prestashop>payment_return_19c419a8a4f1cd621853376a930a2e24'] = 'Eine E-Mail wurde mit diesen Informationen verschickt.';
 $_MODULE['<{emspaybanktransfer}prestashop>payment_return_b9a1cae09e5754424e33764777cfcaa0'] = 'Ihre Bestellung wird verschickt, sobald wir Ihre Zahlung erhalten.';
